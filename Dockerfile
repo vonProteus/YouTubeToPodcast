c425737499874c79c7e04212994b98843571f9b9
Your Name <you@example.com>
2020-06-22 16:13:15 +0100

    now it work in docker

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
-FROM alpine:3
+FROM alpine:edge
 
-ENV YTURL
-ENV ARCHIVEFILE /tmp/arhivefile.txt
+ENV YTURL https://www.youtube.com/channel/UChS9wazLlTUHSTeKVG39hZw
+ENV ARCHIVEFILE /data/youtube-arhive-file.txt
 ENV SKIPARCHIVEFILEINIT 5
 ENV PGAPPDATA /data/
+ENV PGREGENERATERSSURL http://example.com
 
-RUN apk add youtube-dl xmlstarlet ffmpeg jq mutagen 
+RUN apk add youtube-dl bash xmlstarlet ffmpeg jq mutagen curl imagemagick
 
 WORKDIR /scripts/
 
-COPY . /scripts/
+COPY ./scripts/ /scripts/
+
+CMD ./dowload-playlist.sh
--- scripts/process-video.sh
+++ scripts/process-video.sh
@@ -2,30 +2,33 @@
 
 FILE=$1
 ID="${FILE%.*}"
+IDURL=https://www.youtube.com/watch?v=${ID}
 
 echo working on $FILE
 
-JSONID=$(youtube-dl $ID -J)
+JSONID=$(youtube-dl $IDURL -J)
 
 ORGINALURL=$(echo "$JSONID" | jq -r  '.webpage_url')
 TITLE=$(echo "$JSONID" | jq -r  '.title')
 UPLOADER=$(echo "$JSONID" | jq -r  '.uploader')
 SDATE=$(echo "$JSONID" | jq -r '.upload_date')
-DATE=$(date -jf "%Y%m%d" "$SDATE" +"%Y-%m-%d")
+DATE=$(date -d "${SDATE}0000" +"%Y-%m-%d")
 FULLDESCRIPTION=$(echo "$JSONID" | jq -r '.description')
-THUMBNSILURL=$(youtube-dl $ID --get-thumbnail)
+THUMBNSILURL=$(youtube-dl $IDURL --get-thumbnail)
 NEWFILENAME=${DATE}-${TITLE}-${ID}
 NEWFILENAME=${NEWFILENAME///}
 
 curl -o "${NEWFILENAME}.jpg" "$THUMBNSILURL"
+convert "${NEWFILENAME}.jpg" "${NEWFILENAME}.jpg"
 
-cp $FILE "$NEWFILENAME.mp3"
+cp "./$FILE" "$NEWFILENAME.mp3"
 mid3v2 -D "$NEWFILENAME.mp3"
 
-FULL="Orginal Viedo: $ORGINALURL
+FULL="Orginal Viedo: <a href=\"$ORGINALURL\">$ORGINALURL</a>
 
 $FULLDESCRIPTION"
 SHORT=${FULL:0:200}
+SHORT=$FULL
 
 mid3v2 -t "$TITLE" \
        -a "$UPLOADER" \
@@ -36,10 +39,11 @@ mid3v2 -t "$TITLE" \
 
 mid3v2 "$NEWFILENAME.mp3"
 
-SIZE=$(( $(mp3info -p %k "$NEWFILENAME.mp3") / 1024 ))
-DURATION=$(mp3info -p %m:%s "$NEWFILENAME.mp3")
-BITRATE=$(( $(mp3info -p %k "$NEWFILENAME.mp3") * 8 / $(mp3info -p %S "$NEWFILENAME.mp3") ))
-FREQUENCY=$(mp3info -p %Q "$NEWFILENAME.mp3")
+SIZE=$(($(ffprobe -i "$NEWFILENAME.mp3" -show_entries format=size -v quiet -of csv=p=0) / 1024 / 1024))
+DURATION=$(ffprobe -i "$NEWFILENAME.mp3" -show_entries format=duration -v quiet -of csv="p=0" -sexagesimal)
+DURATION=${DURATION%.*}
+BITRATE=$(($(ffprobe -i "$NEWFILENAME.mp3" -show_entries format=bit_rate -v quiet -of csv="p=0") / 1024))
+FREQUENCY=$(ffprobe -show_streams "$NEWFILENAME.mp3" -v quiet -of json | jq -r ".streams[]|select(.codec_name == \"mp3\").sample_rate")
 
 cp template.xml "${NEWFILENAME}.xml"
 
@@ -52,7 +56,7 @@ xml ed -L -u "/PodcastGenerator/episode/categoriesPG/category2PG" --value "" "${
 xml ed -L -u "/PodcastGenerator/episode/categoriesPG/category3PG" --value "" "${NEWFILENAME}.xml"
 xml ed -L -u "/PodcastGenerator/episode/keywordsPG" --value "" "${NEWFILENAME}.xml"
 xml ed -L -u "/PodcastGenerator/episode/explicitPG" --value "no" "${NEWFILENAME}.xml"
-xml ed -L -u "/PodcastGenerator/episode/authorPG/namePG" --value "$UPLOADER" "${NEWFILENAME}.xml"
+xml ed -L -u "/PodcastGenerator/episode/authorPG/namePG" --value "${UPLOADER}" "${NEWFILENAME}.xml"
 xml ed -L -u "/PodcastGenerator/episode/authorPG/emailPG" --value "" "${NEWFILENAME}.xml"
 xml ed -L -u "/PodcastGenerator/episode/fileInfoPG/size" --value "$SIZE" "${NEWFILENAME}.xml"
 xml ed -L -u "/PodcastGenerator/episode/fileInfoPG/duration" --value "$DURATION" "${NEWFILENAME}.xml"
@@ -63,4 +67,13 @@ touch -t ${SDATE}0000 "$NEWFILENAME.mp3"
 touch -t ${SDATE}0000 "$NEWFILENAME.xml"
 touch -t ${SDATE}0000 "$NEWFILENAME.jpg"
 
-# rm "$NEWFILENAME.mp3" "$NEWFILENAME.jpg" "$NEWFILENAME.xml" $FILE
+mkdir -p "${PGAPPDATA}/media"
+mkdir -p "${PGAPPDATA}/images"
+
+cp -p "$NEWFILENAME.mp3" "${PGAPPDATA}/media"
+cp -p "$NEWFILENAME.xml" "${PGAPPDATA}/media"
+cp -p "$NEWFILENAME.jpg" "${PGAPPDATA}/images"
+
+rm "$NEWFILENAME.mp3" "$NEWFILENAME.jpg" "$NEWFILENAME.xml" "./$FILE"
+
+curl $PGREGENERATERSSURL
