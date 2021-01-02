a17c2f4fd2ee86ca4b6c187e9e02334534d39a34
Maciej Krok <>
2021-01-02 20:28:20 +0100

    youtube dl 2020.12.31 and auto update

--- scripts/dowload-playlist.sh
+++ scripts/dowload-playlist.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -xe 
+time {
+	pip install --upgrade youtube_dl
 
-if [ ! -f $ARCHIVEFILE ]; then
-    TMPFILE=$(mktemp)
-    for ID in $(youtube-dl $YTURL --get-id)
-    do
-        echo "youtube $ID" >> $TMPFILE
-    done
-    tail -n +$SKIPARCHIVEFILEINIT $TMPFILE > $ARCHIVEFILE
-fi
-
-youtube-dl $YTURL --download-archive $ARCHIVEFILE  --yes-playlist --limit-rate $LIMITRATE --cookies $COOKIEFILE --no-progress --write-info-json --sleep-interval $MINSLEAP --max-sleep-interval $MAXSELEAP -x --audio-format mp3 --id --exec './process-video.sh {}' $YTDLOPTIONS
+	if [ ! -f $ARCHIVEFILE ]; then
+	    TMPFILE=$(mktemp)
+	    for ID in $(youtube-dl $YTURL --get-id)
+	    do
+	        echo "youtube $ID" >> $TMPFILE
+	    done
+	    tail -n +$SKIPARCHIVEFILEINIT $TMPFILE > $ARCHIVEFILE
+	fi
+	
+	youtube-dl $YTURL --download-archive $ARCHIVEFILE  --yes-playlist --limit-rate $LIMITRATE --cookies $COOKIEFILE --no-progress --write-info-json --sleep-interval $MINSLEAP --max-sleep-interval $MAXSELEAP -x --audio-format mp3 --id --exec './process-video.sh {}' $YTDLOPTIONS
+}
