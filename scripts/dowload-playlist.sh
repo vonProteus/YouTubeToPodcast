12f7255900a0a99d53efe5895f3527df94f55e92
vonProteus <>
2024-04-13 12:20:18 +0200

    better exit code handle

--- scripts/dowload-playlist.sh
+++ scripts/dowload-playlist.sh
@@ -20,10 +20,23 @@ time {
       tail -n +$SKIPARCHIVEFILEINIT $TMPFILE >$ARCHIVEFILE
    fi
 
-   youtube-dl $YTURL --download-archive $ARCHIVEFILE --yes-playlist --limit-rate $LIMITRATE --cookies $COOKIEFILE --no-progress --write-info-json --sleep-interval $MINSLEAP --max-sleep-interval $MAXSELEAP -x --audio-format mp3 --id --exec 'process-video.sh {}' $YTDLOPTIONS
-   YT_EXIT_CODE=$?
+   youtube-dl $YTURL \
+      --download-archive $ARCHIVEFILE \
+      --yes-playlist \
+      --limit-rate $LIMITRATE \
+      --cookies $COOKIEFILE \
+      --no-progress \
+      --write-info-json \
+      --sleep-interval $MINSLEAP \
+      --max-sleep-interval $MAXSELEAP \
+      --extract-audio \
+      --audio-format mp3 \
+      --id \
+      --exec 'process-video.sh {}' \
+      $YTDLOPTIONS \
+      || YT_EXIT_CODE=$?
 
    curl $PGREGENERATERSSURL || echo "notifying PodcastGenerator failed…"
 
-   exit $YT_EXIT_CODE
+   exit ${YT_EXIT_CODE:=0}
 }
